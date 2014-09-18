d639413bf2b06c47ffa14cf3e2fe8c70073faa76
Craig Phillips <iwonbigbro@gmail.com>
2014-09-18 15:16:27 +0100

    [JENKINS-24076] Add a configuration option that allows you to force Cppcheck to compare results against a stable build rather than the last build.

--- src/main/java/org/jenkinsci/plugins/cppcheck/CppcheckPublisher.java
+++ src/main/java/org/jenkinsci/plugins/cppcheck/CppcheckPublisher.java
@@ -42,6 +42,7 @@ public class CppcheckPublisher extends Recorder {
     public CppcheckPublisher(String pattern,
                              boolean ignoreBlankFiles, String threshold,
                              boolean allowNoReport,
+                             boolean stableBuild,
                              String newThreshold, String failureThreshold,
                              String newFailureThreshold, String healthy, String unHealthy,
                              boolean severityError,
@@ -66,6 +67,7 @@ public class CppcheckPublisher extends Recorder {
         cppcheckConfig.setPattern(pattern);
         cppcheckConfig.setAllowNoReport(allowNoReport);
         cppcheckConfig.setIgnoreBlankFiles(ignoreBlankFiles);
+        cppcheckConfig.setStableBuild(stableBuild);
         CppcheckConfigSeverityEvaluation configSeverityEvaluation = new CppcheckConfigSeverityEvaluation(
                 threshold, newThreshold, failureThreshold, newFailureThreshold, healthy, unHealthy,
                 severityError,
@@ -143,7 +145,8 @@ public class CppcheckPublisher extends Recorder {
                     = new CppcheckSourceContainer(listener, build.getWorkspace(),
                             build.getModuleRoot(), cppcheckReport.getAllErrors());
 
-            CppcheckResult result = new CppcheckResult(cppcheckReport.getStatistics(), build);
+            CppcheckResult result = new CppcheckResult(cppcheckReport.getStatistics(), build, 
+            		cppcheckConfig.getStableBuild());
             CppcheckConfigSeverityEvaluation severityEvaluation
                     = cppcheckConfig.getConfigSeverityEvaluation();
 
